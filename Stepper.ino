ae705e33c33717ac23352aad2c69f8a024839924
pJEAN2002 <p.jeanmougin@debrouillonet.org>
2022-12-10 16:52:45 +0100

    Stepper

--- Stepper.ino
+++ Stepper.ino
@@ -1,22 +1,22 @@
 #include <Stepper.h>
 
-
 #define limitSwitch1 11
 #define limitSwitch2 10
 #define limitSwitch3 9
 #define limitSwitch4 A3
-Stepper PasPas = Stepper(200,12,13);
+
+Stepper PasPas = Stepper(200,4,7);
 
 void setup()
 {
     Serial.begin(9600);
-    pinMode(limitSwitch1, INPUT_PULLUP);
-    pinMode(limitSwitch2, INPUT_PULLUP);
-    pinMode(limitSwitch3, INPUT_PULLUP);
-    pinMode(limitSwitch4, INPUT_PULLUP);
+    PasPas.setSpeed(200);
 }
 
 void loop()
 {
-	
+	PasPas.step(2000);
+    delay(1000);
+    PasPas.step(-2000);
+    delay(1000);
 }
